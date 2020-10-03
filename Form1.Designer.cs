c52d027e215458377fca58d76e39f0eab4eb1ad0
Maksims Garipovs <maksim.garipov@gmail.com>
2020-10-04 00:14:07 +0300

    first fraft using XmlReader and temporary variables

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnGetRate = new System.Windows.Forms.Button();
+            this.tmpLabel = new System.Windows.Forms.Label();
+            this.tmpLabel2 = new System.Windows.Forms.Label();
+            this.tmpLabel3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGetRate
+            // 
+            this.btnGetRate.Location = new System.Drawing.Point(380, 266);
+            this.btnGetRate.Name = "btnGetRate";
+            this.btnGetRate.Size = new System.Drawing.Size(75, 23);
+            this.btnGetRate.TabIndex = 0;
+            this.btnGetRate.Text = "Get rate";
+            this.btnGetRate.UseVisualStyleBackColor = true;
+            this.btnGetRate.Click += new System.EventHandler(this.btnGetRate_Click);
+            // 
+            // tmpLabel
+            // 
+            this.tmpLabel.AutoSize = true;
+            this.tmpLabel.Location = new System.Drawing.Point(377, 220);
+            this.tmpLabel.Name = "tmpLabel";
+            this.tmpLabel.Size = new System.Drawing.Size(35, 13);
+            this.tmpLabel.TabIndex = 1;
+            this.tmpLabel.Text = "label1";
+            // 
+            // tmpLabel2
+            // 
+            this.tmpLabel2.AutoSize = true;
+            this.tmpLabel2.Location = new System.Drawing.Point(377, 316);
+            this.tmpLabel2.Name = "tmpLabel2";
+            this.tmpLabel2.Size = new System.Drawing.Size(35, 13);
+            this.tmpLabel2.TabIndex = 2;
+            this.tmpLabel2.Text = "label2";
+            // 
+            // tmpLabel3
+            // 
+            this.tmpLabel3.AutoSize = true;
+            this.tmpLabel3.Location = new System.Drawing.Point(377, 329);
+            this.tmpLabel3.Name = "tmpLabel3";
+            this.tmpLabel3.Size = new System.Drawing.Size(35, 13);
+            this.tmpLabel3.TabIndex = 3;
+            this.tmpLabel3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tmpLabel3);
+            this.Controls.Add(this.tmpLabel2);
+            this.Controls.Add(this.tmpLabel);
+            this.Controls.Add(this.btnGetRate);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGetRate;
+        private System.Windows.Forms.Label tmpLabel;
+        private System.Windows.Forms.Label tmpLabel2;
+        private System.Windows.Forms.Label tmpLabel3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml; // allow using Xml
 
 namespace EcbCurrencyRates
 {
@@ -15,6 +16,60 @@ namespace EcbCurrencyRates
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnGetRate_Click(object sender, EventArgs e)
+        {
+            // temporary things
+            String TempCur = "HKD";
+            String TempDate = "20200925";
+            tmpLabel.Text = TempCur;
+
+            // Set the reader settings
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.IgnoreComments = true;
+            settings.IgnoreProcessingInstructions = true;
+            settings.IgnoreWhitespace = true;
+
+            // Create XmlReader object
+            String UrlString = "https://www.bank.lv/vk/ecb.xml" + "?date=" + TempDate;
+            XmlReader reader = XmlReader.Create(UrlString, settings);
+            
+            using (reader)
+            {
+                reader.MoveToContent();
+                reader.ReadToFollowing("Currency");
+                do
+                {
+                    XmlReader inner = reader.ReadSubtree();
+                    inner.ReadToDescendant("ID");
+                    //tmpLabel2.Text = inner.ReadElementContentAsString();
+                    //tmpLabel3.Text = inner.ReadElementContentAsString();
+                    if (inner.ReadElementContentAsString() == TempCur)
+                    {
+                        if (inner.Name != "Rate")
+                        {
+                            inner.ReadToDescendant("Rate");
+                            tmpLabel2.Text = inner.Name + " if";
+                            tmpLabel3.Text = inner.ReadElementContentAsString();
+                        }
+                        else
+                        {
+                            //tmpLabel2.Text = inner.NodeType.ToString();
+                            tmpLabel2.Text = inner.Name + " else";
+                            tmpLabel3.Text = inner.ReadElementContentAsString();
+                        }
+                        inner.Close();
+                        break;
+                    }
+                    else
+                    {
+                        inner.Close();
+                        continue;
+                    }
+                } while (reader.ReadToNextSibling("Currency"));
+            }
+            reader.Close();
         }
     }
 }
