333d633338aea9282b418e4d417f1508fa750e88
Maksims Garipovs <maksim.garipov@gmail.com>
2020-10-05 22:50:38 +0300

    finished task

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,9 +29,6 @@
         private void InitializeComponent()
         {
             this.btnGetRate = new System.Windows.Forms.Button();
-            this.tmpLabel = new System.Windows.Forms.Label();
-            this.tmpLabel2 = new System.Windows.Forms.Label();
-            this.tmpLabel3 = new System.Windows.Forms.Label();
             this.tbCurCode = new System.Windows.Forms.TextBox();
             this.dtpDate = new System.Windows.Forms.DateTimePicker();
             this.tbResult = new System.Windows.Forms.TextBox();
@@ -45,63 +42,41 @@
             this.btnGetRate.Location = new System.Drawing.Point(94, 64);
             this.btnGetRate.Name = "btnGetRate";
             this.btnGetRate.Size = new System.Drawing.Size(197, 23);
-            this.btnGetRate.TabIndex = 0;
+            this.btnGetRate.TabIndex = 2;
             this.btnGetRate.Text = "Get rate";
             this.btnGetRate.UseVisualStyleBackColor = true;
             this.btnGetRate.Click += new System.EventHandler(this.btnGetRate_Click);
             // 
-            // tmpLabel
-            // 
-            this.tmpLabel.AutoSize = true;
-            this.tmpLabel.Location = new System.Drawing.Point(297, 15);
-            this.tmpLabel.Name = "tmpLabel";
-            this.tmpLabel.Size = new System.Drawing.Size(35, 13);
-            this.tmpLabel.TabIndex = 1;
-            this.tmpLabel.Text = "label1";
-            // 
-            // tmpLabel2
-            // 
-            this.tmpLabel2.AutoSize = true;
-            this.tmpLabel2.Location = new System.Drawing.Point(297, 69);
-            this.tmpLabel2.Name = "tmpLabel2";
-            this.tmpLabel2.Size = new System.Drawing.Size(35, 13);
-            this.tmpLabel2.TabIndex = 2;
-            this.tmpLabel2.Text = "label2";
-            // 
-            // tmpLabel3
-            // 
-            this.tmpLabel3.AutoSize = true;
-            this.tmpLabel3.Location = new System.Drawing.Point(297, 96);
-            this.tmpLabel3.Name = "tmpLabel3";
-            this.tmpLabel3.Size = new System.Drawing.Size(35, 13);
-            this.tmpLabel3.TabIndex = 3;
-            this.tmpLabel3.Text = "label3";
-            // 
             // tbCurCode
             // 
+            this.tbCurCode.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
             this.tbCurCode.Location = new System.Drawing.Point(94, 12);
+            this.tbCurCode.MaxLength = 3;
             this.tbCurCode.Name = "tbCurCode";
             this.tbCurCode.Size = new System.Drawing.Size(197, 20);
-            this.tbCurCode.TabIndex = 4;
+            this.tbCurCode.TabIndex = 0;
             this.tbCurCode.TextChanged += new System.EventHandler(this.tbCurCode_TextChanged);
+            this.tbCurCode.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbCurCode_KeyPress);
             // 
             // dtpDate
             // 
             this.dtpDate.CustomFormat = "dddd,MMMM d,yyyy";
             this.dtpDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this.dtpDate.Location = new System.Drawing.Point(94, 38);
+            this.dtpDate.MinDate = new System.DateTime(1999, 1, 4, 0, 0, 0, 0);
             this.dtpDate.Name = "dtpDate";
             this.dtpDate.Size = new System.Drawing.Size(197, 20);
-            this.dtpDate.TabIndex = 5;
+            this.dtpDate.TabIndex = 1;
+            this.dtpDate.Value = new System.DateTime(2020, 10, 5, 0, 0, 0, 0);
             this.dtpDate.ValueChanged += new System.EventHandler(this.dtpDate_ValueChanged);
             // 
             // tbResult
             // 
             this.tbResult.Location = new System.Drawing.Point(94, 93);
             this.tbResult.Name = "tbResult";
+            this.tbResult.ReadOnly = true;
             this.tbResult.Size = new System.Drawing.Size(197, 20);
-            this.tbResult.TabIndex = 6;
-            this.tbResult.TextChanged += new System.EventHandler(this.tbResult_TextChanged);
+            this.tbResult.TabIndex = 3;
             // 
             // lblCurCode
             // 
@@ -109,9 +84,8 @@
             this.lblCurCode.Location = new System.Drawing.Point(12, 15);
             this.lblCurCode.Name = "lblCurCode";
             this.lblCurCode.Size = new System.Drawing.Size(76, 13);
-            this.lblCurCode.TabIndex = 7;
+            this.lblCurCode.TabIndex = 4;
             this.lblCurCode.Text = "Currency code";
-            this.lblCurCode.Click += new System.EventHandler(this.lblCurCode_Click);
             // 
             // lblDate
             // 
@@ -119,7 +93,7 @@
             this.lblDate.Location = new System.Drawing.Point(58, 41);
             this.lblDate.Name = "lblDate";
             this.lblDate.Size = new System.Drawing.Size(30, 13);
-            this.lblDate.TabIndex = 8;
+            this.lblDate.TabIndex = 5;
             this.lblDate.Text = "Date";
             // 
             // lblResult
@@ -128,24 +102,22 @@
             this.lblResult.Location = new System.Drawing.Point(51, 96);
             this.lblResult.Name = "lblResult";
             this.lblResult.Size = new System.Drawing.Size(37, 13);
-            this.lblResult.TabIndex = 9;
+            this.lblResult.TabIndex = 6;
             this.lblResult.Text = "Result";
             // 
             // RateForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(375, 127);
+            this.ClientSize = new System.Drawing.Size(304, 127);
             this.Controls.Add(this.lblResult);
             this.Controls.Add(this.lblDate);
             this.Controls.Add(this.lblCurCode);
             this.Controls.Add(this.tbResult);
             this.Controls.Add(this.dtpDate);
             this.Controls.Add(this.tbCurCode);
-            this.Controls.Add(this.tmpLabel3);
-            this.Controls.Add(this.tmpLabel2);
-            this.Controls.Add(this.tmpLabel);
             this.Controls.Add(this.btnGetRate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "RateForm";
             this.Text = "Rates";
             this.Load += new System.EventHandler(this.RateForm_Load);
@@ -157,9 +129,6 @@
         #endregion
 
         private System.Windows.Forms.Button btnGetRate;
-        private System.Windows.Forms.Label tmpLabel;
-        private System.Windows.Forms.Label tmpLabel2;
-        private System.Windows.Forms.Label tmpLabel3;
         private System.Windows.Forms.TextBox tbCurCode;
         private System.Windows.Forms.DateTimePicker dtpDate;
         private System.Windows.Forms.TextBox tbResult;
--- Form1.cs
+++ Form1.cs
@@ -6,102 +6,103 @@ namespace EcbCurrencyRates
 {
     public partial class RateForm : Form
     {
-        private string inputCurCode;
-        private string inputDate;
+        private string baseUrlString = "https://www.bank.lv/vk/ecb.xml"; // base service URL
+        private string inputCurCode; // for currency code input value
+        private string inputDate; // for date input value
+        private string curCode; // for comparisson of currency codes in the xml
+        private string outputRate; // for currency rate output
+
+        // Set the reader settings
+        private XmlReaderSettings SetXmlReaderSettings()
+        {
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.IgnoreComments = true;
+            settings.IgnoreProcessingInstructions = true;
+            settings.IgnoreWhitespace = true;
+
+            return settings;
+        }
 
         public RateForm()
         {
             InitializeComponent();
         }
 
-        private void btnGetRate_Click(object sender, EventArgs e)
+        private void RateForm_Load(object sender, EventArgs e)
         {
-            // temporary things
-            //string tempCur = "HKD";
-            //string tempDate = "20200925";
-            //tmpLabel.Text = tempCur;
-            tmpLabel.Text = inputCurCode;
-
-            tbResult.Text = ""; //removing any old value
-
-            // Set the reader settings
-            XmlReaderSettings settings = new XmlReaderSettings();
-            settings.IgnoreComments = true;
-            settings.IgnoreProcessingInstructions = true;
-            settings.IgnoreWhitespace = true;
-
-            // Create XmlReader object
-            string urlString = "https://www.bank.lv/vk/ecb.xml" + "?date=" + inputDate;
-            XmlReader reader = XmlReader.Create(urlString, settings);
-
-            using (reader)
-            {
-                reader.MoveToContent();
-                reader.ReadToFollowing("Currency");
-                do
-                {
-                    XmlReader inner = reader.ReadSubtree();
-                    inner.ReadToDescendant("ID");
-                    //tmpLabel2.Text = inner.ReadElementContentAsString();
-                    //tmpLabel3.Text = inner.ReadElementContentAsString();
-                    string curCode = inner.ReadElementContentAsString();
-                    //if (curCode == tempCur)
-                    if (curCode == inputCurCode)
-                    {
-                        if (inner.Name != "Rate")
-                        {
-                            inner.ReadToDescendant("Rate");
-                            tmpLabel2.Text = inner.Name + " if";
-                            string outputRate = inner.ReadElementContentAsString();
-                            tmpLabel3.Text = outputRate;
-                        }
-                        else
-                        {
-                            //tmpLabel2.Text = inner.NodeType.ToString();
-                            tmpLabel2.Text = inner.Name + " else";
-                            string outputRate = inner.ReadElementContentAsString();
-                            tmpLabel3.Text = outputRate;
-                            tbResult.Text = outputRate;
-                        }
-                        inner.Close();
-                        break;
-                    }
-                    else
-                    {
-                        inner.Close();
-                        continue;
-                    }
-                } while (reader.ReadToNextSibling("Currency"));
-            }
-            reader.Close();
-        }
-
-        private void lblCurCode_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void tbResult_TextChanged(object sender, EventArgs e)
-        {
-
+            dtpDate.MaxDate = DateTime.Now.Date; // do not allow dates greater than today
+            inputDate = dtpDate.Value.Date.ToString("yyyyMMdd"); // read initial value from the date/time picker
         }
 
         private void dtpDate_ValueChanged(object sender, EventArgs e)
         {
-            inputDate = dtpDate.Value.Date.ToString("yyyyMMdd");
-            //MessageBox.Show(inputDate);
+            inputDate = dtpDate.Value.Date.ToString("yyyyMMdd"); // read value from the date/time picker on change
         }
 
-        private void RateForm_Load(object sender, EventArgs e)
+        // only allow letters, backspace and ctrl when typing in the "Currency code" field
+        private void tbCurCode_KeyPress(object sender, KeyPressEventArgs e)
         {
-            inputDate = dtpDate.Value.Date.ToString("yyyyMMdd");
-            //MessageBox.Show(inputDate);
+            if (!(char.IsLetter(e.KeyChar)) && !(e.KeyChar == (char)Keys.Back) && !(char.IsControl(e.KeyChar)))
+            {
+                e.Handled = true;
+            }
         }
 
         private void tbCurCode_TextChanged(object sender, EventArgs e)
         {
-            //string inputCurCode = tbCurCode.Text.ToUpper();
-            inputCurCode = tbCurCode.Text.ToUpper();
+            //inputCurCode = tbCurCode.Text.ToUpper();
+            inputCurCode = tbCurCode.Text; // read input value from the "Currency code" form field on change
+        }
+
+        private void btnGetRate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(inputCurCode))
+            {
+                MessageBox.Show("Please enter a currency code.");
+                return;
+            }
+
+            tbResult.Text = ""; // remove old value from the "Result" form field
+            outputRate = null;
+            XmlReaderSettings settings = SetXmlReaderSettings();
+
+            // Create XmlReader object
+            string urlString = baseUrlString + "?date=" + inputDate;
+            XmlReader reader = XmlReader.Create(urlString, settings);
+
+            using (reader)
+            {
+                //reader.MoveToContent(); // skipping to actual xml content
+                reader.ReadToFollowing("Currency"); // go to first "Currency" xml element
+                do
+                {
+                    XmlReader inner = reader.ReadSubtree(); // create XmlReader object to read current "Currency" xml element
+                    inner.ReadToDescendant("ID"); // go to "ID" xml element
+                    curCode = inner.ReadElementContentAsString(); // read "ID" value and move the reader past the element end node
+                    
+                    if (curCode.Equals(inputCurCode, StringComparison.OrdinalIgnoreCase)) // compare ID and currency code input value ignoring case
+                    {
+                        // in particular case of this task we know that "Rate" xml element comes right after "ID"
+                        // otherwise additional checks could have been added
+                        outputRate = inner.ReadElementContentAsString(); // read "Rate" value and move the reader past the element end node
+                        tbResult.Text = outputRate; // output "Rate" to "Result" field of the form
+                        inner.Close(); // close inner reader
+                        break; // move out of loop
+                    }
+                    else
+                    {
+                        inner.Close(); // close inner reader
+                        continue; // continue to next loop iteration
+                    }
+                } while (reader.ReadToNextSibling("Currency")); // loop while there are "Currency" elements left in the xml
+            }
+            reader.Close(); // close outer reader
+
+            if (outputRate == null)
+            {
+                MessageBox.Show("Currency rate for the provided currency code could not be found. Please check, if currency code is correct.");
+                return;
+            }
         }
     }
 }
